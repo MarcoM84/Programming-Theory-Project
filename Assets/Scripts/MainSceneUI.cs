a3066435248e382d201cc94b88caae20364ac1ca
MarcoM84 <107421239+MarcoM84@users.noreply.github.com>
2022-06-30 17:35:05 +0200

    Main scene
    added main scene and button to menu

---
+++ Assets/Scripts/MainSceneUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneUI : MonoBehaviour
+{
+    public void MenuClicked()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
