6d464ac9d1db43d7237f7aa9eee3b02b53c2cfeb
MarcoM84 <107421239+MarcoM84@users.noreply.github.com>
2022-06-30 18:48:32 +0200

    Final commit

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shape : MonoBehaviour
+public class Shape : MonoBehaviour // INHERITANCE
 {
     public string message { get; protected set; } // ENCAPSULATION
 
